37d30244c9762c38b511d5a36641abec88b2dbbf
mfullerton4 <mfullerton4@gmail.com>
2025-07-02 20:04:48 -0600

    I wrote the number guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guessNum = 0;
+
+            while (guess != magicNum)
+            {
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessNum++;
+
+                if (guess < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {guessNum} tries.");
+                }
+            }
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }
+        Console.WriteLine("Thanks for playing!");
     }
 }
