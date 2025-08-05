e7179eff18af1d06d95a20bf42a26f6d97e79489
mfullerton4 <mfullerton4@gmail.com>
2025-08-05 08:54:09 -0600

    I wrote the Shapes program.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("green", 6));
+        shapes.Add(new Rectangle("blue", 5, 9));
+        shapes.Add(new Circle("purple", 5));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
